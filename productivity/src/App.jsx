20d9559ed1e592e66bf31668fe2daa2529778bd2
Manuel Cervera <manuelcerveralara@gmail.com>
2025-06-06 13:03:37 +0200

    hoal

--- productivity/src/App.jsx
+++ productivity/src/App.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import Temp from "./temp.jsx";
 
 export default function App() {
-  return <Temp />;
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-200">
+        ¡Built-in Tailwind Funciona!
+      </h1>
+    </div>
+  );
 }
